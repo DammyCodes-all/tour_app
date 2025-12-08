6501d98462b2cd25dab679a9a57e86faf3e3cbe7
DammyCodes-all <harliarmeen@gmail.com>
2025-12-08 15:28:13 +0100

    feat: implement login and signup pages, enhance styling, and add button component

---
+++ web_app/app/(auth)/login/page.tsx
@@ -0,0 +1,10 @@
+const LoginPage = () => {
+  return (
+    <div className="flex min-h-screen w-full justify-center md:justify-between items-center bg-theme-black gap-5">
+      <div className="flex-1 "></div>
+      <div className="flex-1 bg-theme-dark-orange rounded-2xl"></div>
+    </div>
+  );
+};
+
+export default LoginPage;
